36d1fa5ee00ebc4c034aed85fb3163f3078b374f
Natalie Chanfreau <nataliechanfreau@gmail.com>
2015-05-19 00:01:53 -0400

    changed background colors to be lighter, deleted unused code

--- app/src/main/java/com/example/nataliechanfreau/myapplication/FirstActivity.java
+++ app/src/main/java/com/example/nataliechanfreau/myapplication/FirstActivity.java
@@ -1,98 +1,40 @@
 package com.example.nataliechanfreau.myapplication;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class FirstActivity extends ActionBarActivity {
     public static final String EXTRA_MESSAGE = "com.example.nataliechanfreau.myapplication.MESSAGE";
     private static final int MAX_SECTIONS = 6;
+    private static final int PROPORTION_AMT = 100;
+    private static final int GRADE_AMT = 200;
+    private static final float ROW_TEXT_SIZE = 25;
     private static final String SECTION_STRING = "Section ";
     private static final String PERCENT_SYMBOL = "%";
     private static final String SAMPLE_PROPORTION = "20";
     private static final String SAMPLE_GRADE = "88";
-    private static final int PROPORTION_AMT = 100;
-    private static final int GRADE_AMT = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
-        //createAdderButton();
-        //letsTrySomething(5);
     }
 
-    private void letsTrySomething(int num) {
-        for (int i = 1; i <= num; i++) {
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT);
-            Button btn = new Button(this);
-            btn.setId(i);
-            final int id_ = btn.getId();
-            btn.setText("button " + id_);
-            ((LinearLayout) findViewById(R.id.linearLayout1)).addView(btn, params);
-            Button btn1 = ((Button) findViewById(id_));
-            btn1.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    Toast.makeText(view.getContext(),
-                            "Button clicked index = " + id_, Toast.LENGTH_SHORT)
-                            .show();
-                }
-            });
-        }
-    }
-
-    private void createHeaderRow() {
-
-    }
-
-    private void createRow() {
-        Button b = new Button(this);
-        b.setText("here's the row");
-        RelativeLayout ll = (RelativeLayout)findViewById(R.id.main_relative);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        ll.addView(b, lp);
-    }
-
-    private void createAdderButton() {
-        Button b = new Button(this);
-        b.setText("Add new class");
-        b.setOnClickListener(getAdderClickListener());
-        RelativeLayout ll = (RelativeLayout)findViewById(R.id.main_relative);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        ll.addView(b, lp);
-    }
-
-    private View.OnClickListener getAdderClickListener() {
-        View.OnClickListener ocl = new View.OnClickListener() {
-            public void onClick(View v) {
-                createRow();
-            }
-        };
-        return ocl;
-    }
-
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
+    }*/
 
     public void buttonOnClick(View v) {
         Button b = (Button) v;
@@ -114,15 +56,15 @@ public class FirstActivity extends ActionBarActivity {
         }
         ((LinearLayout) findViewById(R.id.linearLayout1)).removeAllViews();
         for (int i = 1; i <= num; i++) {
-            LinearLayout row = linearLayout();
+            LinearLayout row = createLinearLayout();
 
-            TextView section = makeSimpleTextView(SECTION_STRING + i);
+            TextView section = makeSimpleTextView(SECTION_STRING + i, ROW_TEXT_SIZE);
             EditText proportion = makeEditText(PROPORTION_AMT + i, SAMPLE_PROPORTION,
-                    InputType.TYPE_CLASS_NUMBER);
-            TextView percent = makeSimpleTextView(PERCENT_SYMBOL);
+                    InputType.TYPE_CLASS_NUMBER, ROW_TEXT_SIZE);
+            TextView percent = makeSimpleTextView(PERCENT_SYMBOL, ROW_TEXT_SIZE);
             EditText grade = makeEditText(GRADE_AMT + i, SAMPLE_GRADE,
-                    InputType.TYPE_CLASS_NUMBER);
-            TextView percent2 = makeSimpleTextView(PERCENT_SYMBOL);
+                    InputType.TYPE_CLASS_NUMBER, ROW_TEXT_SIZE);
+            TextView percent2 = makeSimpleTextView(PERCENT_SYMBOL, ROW_TEXT_SIZE);
 
             //final int id_ = btn.getId();
             //((LinearLayout) findViewById(R.id.linearLayout1)).addView(tv);
@@ -153,22 +95,24 @@ public class FirstActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private LinearLayout linearLayout(){
+    private LinearLayout createLinearLayout(){
         LinearLayout linearLayout = new LinearLayout(this);
         linearLayout.setOrientation(LinearLayout.HORIZONTAL);
         return linearLayout;
     }
 
-    private TextView makeSimpleTextView(String content) {
+    private TextView makeSimpleTextView(String content, float textSize) {
         TextView tv = new TextView(this);
         tv.setText(content);
+        tv.setTextSize(textSize);
         return tv;
     }
 
-    private EditText makeEditText(int id, String text, int inputType) {
+    private EditText makeEditText(int id, String text, int inputType, float textSize) {
         EditText et = new EditText(this);
         et.setId(id);
         et.setText(text);
+        et.setTextSize(textSize);
         et.setInputType(inputType);
         et.setSelectAllOnFocus(true);
         return et;
