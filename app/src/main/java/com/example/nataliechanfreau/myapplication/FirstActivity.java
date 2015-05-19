b29a14852d85f862f9710caeb5a884988ad30a6e
Natalie Chanfreau <nataliechanfreau@gmail.com>
2015-05-19 00:54:36 -0400

    added math setup to first activity

--- app/src/main/java/com/example/nataliechanfreau/myapplication/FirstActivity.java
+++ app/src/main/java/com/example/nataliechanfreau/myapplication/FirstActivity.java
@@ -19,10 +19,15 @@ public class FirstActivity extends ActionBarActivity {
     private static final int PROPORTION_AMT = 100;
     private static final int GRADE_AMT = 200;
     private static final float ROW_TEXT_SIZE = 25;
+    private static final float TITLE_TEXT_SIZE = 20;
     private static final String SECTION_STRING = "Section ";
+    private static final String PROPORTION_STRING = "Proportion";
+    private static final String GRADE_STRING = "Grade";
     private static final String PERCENT_SYMBOL = "%";
     private static final String SAMPLE_PROPORTION = "20";
     private static final String SAMPLE_GRADE = "88";
+
+    private int myNumRows;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,22 +38,65 @@ public class FirstActivity extends ActionBarActivity {
     public void buttonOnClick(View v) {
         Button b = (Button) v;
 
+        double[] result = doMathOnUserInput();
+
+        double currentPercent = result[0];
+        double remainingProportion = result[1];
+
         Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, "teehee");
+        intent.putExtra(EXTRA_MESSAGE, currentPercent + " " + remainingProportion);
         startActivity(intent);
     }
 
+    private double[] doMathOnUserInput() {
+        double[] ret = new double[2];
+
+        double currentPercent = 0;
+        double remainingProportion = 100;
+
+        for (int i = 1; i <= myNumRows; i++) {
+            double proportion = Double.parseDouble(((EditText) findViewById(PROPORTION_AMT + i)).
+                    getText().toString());
+            double grade = Double.parseDouble(((EditText) findViewById(GRADE_AMT + i)).
+                    getText().toString());
+
+            remainingProportion -= proportion;
+            currentPercent += 0.01 * proportion * grade;
+
+        }
+
+        ret[0] = currentPercent;
+        ret[1] = remainingProportion;
+
+        return ret;
+    }
+
     public void classesButtonOnClick(View v) {
+        clearRows();
         addTitleRow();
-        int num = calculateNumRows();
-        addSectionRows(num);
+        calculateNumRows();
+        addSectionRows(myNumRows);
+    }
+
+    private void clearRows() {
+        ((LinearLayout) findViewById(R.id.linearLayout1)).removeAllViews();
     }
 
     private void addTitleRow() {
-
+        LinearLayout titleRow = createTitleRow();
+        ((LinearLayout) findViewById(R.id.linearLayout1)).addView(titleRow);
     }
 
-    private int calculateNumRows() {
+    private LinearLayout createTitleRow() {
+        LinearLayout row = createLinearLayout();
+        String title = SECTION_STRING + "\t" + PROPORTION_STRING + "\t" + GRADE_STRING;
+        TextView titleView = makeSimpleTextView(title, TITLE_TEXT_SIZE);
+        addToViewGroup(row, titleView);
+
+        return row;
+    }
+
+    private void calculateNumRows() {
         EditText classesText = (EditText) this.findViewById(R.id.classesText);
         int num = Integer.parseInt(classesText.getText().toString());
 
@@ -57,27 +105,29 @@ public class FirstActivity extends ActionBarActivity {
             num = MAX_SECTIONS;
         }
 
-        return num;
+        myNumRows = num;
     }
 
     private void addSectionRows(int num) {
-        ((LinearLayout) findViewById(R.id.linearLayout1)).removeAllViews();
         for (int i = 1; i <= num; i++) {
-            LinearLayout row = createLinearLayout();
-
-            TextView section = makeSimpleTextView(SECTION_STRING + i, ROW_TEXT_SIZE);
-            EditText proportion = makeEditText(PROPORTION_AMT + i, SAMPLE_PROPORTION,
-                    InputType.TYPE_CLASS_NUMBER, ROW_TEXT_SIZE);
-            TextView percent = makeSimpleTextView(PERCENT_SYMBOL, ROW_TEXT_SIZE);
-            EditText grade = makeEditText(GRADE_AMT + i, SAMPLE_GRADE,
-                    InputType.TYPE_CLASS_NUMBER, ROW_TEXT_SIZE);
-            TextView percent2 = makeSimpleTextView(PERCENT_SYMBOL, ROW_TEXT_SIZE);
-
-            addToViewGroup(row, section, proportion, percent, grade, percent2);
-
+            LinearLayout row = createSingleSectionRow(i);
             ((LinearLayout) findViewById(R.id.linearLayout1)).addView(row);
-
         }
+    }
+
+    private LinearLayout createSingleSectionRow(int i) {
+        LinearLayout row = createLinearLayout();
+        TextView section = makeSimpleTextView(SECTION_STRING + i, ROW_TEXT_SIZE);
+        EditText proportion = makeEditText(PROPORTION_AMT + i, SAMPLE_PROPORTION,
+                InputType.TYPE_CLASS_NUMBER, ROW_TEXT_SIZE);
+        TextView percent = makeSimpleTextView(PERCENT_SYMBOL, ROW_TEXT_SIZE);
+        EditText grade = makeEditText(GRADE_AMT + i, SAMPLE_GRADE,
+                InputType.TYPE_CLASS_NUMBER, ROW_TEXT_SIZE);
+        TextView percent2 = makeSimpleTextView(PERCENT_SYMBOL, ROW_TEXT_SIZE);
+
+        addToViewGroup(row, section, proportion, percent, grade, percent2);
+
+        return row;
     }
 
     private void addToViewGroup(ViewGroup view, View ... children) {
